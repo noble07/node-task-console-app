2ed56df70c3d58d16027e94f346edc2a1ef59583
Juan Camilo <juancmz98@gmail.com>
2021-09-15 21:55:24 -0500

    Inquierer package added to create the interactive menu

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('colors')
-
-const { mostrarMenu, pausa } = require('./helpers/mensajes')
+const { inquiererMenu } = require('./helpers/inquirer')
 
 console.clear()
 
@@ -12,10 +11,8 @@ const main = async() => {
 
   do {
 
-   opt = await mostrarMenu()
+   opt = await inquiererMenu()
    console.log({opt})
-
-   if (opt !== '0') await pausa()
 
   } while (opt !== '0');
 
---
+++ helpers/inquirer.js
@@ -0,0 +1,28 @@
+const inquirer = require('inquirer');
+require('colors')
+
+const questions = [
+  {
+    type: 'list',
+    name: 'option',
+    message: '¿Qué desea hacer?',
+    choices: ['opt1', 'opt2', 'opt3']
+  }
+]
+
+const inquiererMenu = async() => {
+
+  console.clear();
+  console.log('==========================='.green)
+  console.log('   Seleccione una opción')
+  console.log('===========================\n'.green)
+
+  const opt = await inquirer.prompt(questions)
+
+  return opt
+
+}
+
+module.exports = {
+  inquiererMenu
+}
